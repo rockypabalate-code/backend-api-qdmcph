792db840339588694ee18b620461a73f54b68b3c
Rockz <rocky.pabalate@qdmcph.com>
2026-06-09 21:49:48 +0800

    add cors

--- src/app.js
+++ src/app.js
@@ -5,6 +5,18 @@ const overtimeRoutes = require('./routes/overtimeRoutes');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
